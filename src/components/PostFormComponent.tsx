9a397589124e56b22f03566d6b1ff9dc14a335ce
Koval89 <alexandrgalaxy4@gmail.com>
2024-10-22 17:34:39 +0300

    HW5

--- src/components/PostFormComponent.tsx
+++ src/components/PostFormComponent.tsx
@@ -24,7 +24,6 @@ const PostFormComponent = () => {
 
 
     const customHendler = (dataFromForm: PostProps) => {
-        newPost(dataFromForm).then(response=> console.log(response))
         console.log(dataFromForm)
     }
 
